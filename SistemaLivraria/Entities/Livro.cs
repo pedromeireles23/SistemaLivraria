6330e82237bf6f6a6df26ae7f76cc35de5e348e8
pedromeireles23 <pemeirels@gmail.com>
2026-05-01 15:59:45 -0300

    passo 01 comeco da criação da classe Livro

--- SistemaLivraria/Entities/Livro.cs
+++ SistemaLivraria/Entities/Livro.cs
@@ -1,4 +1,45 @@
-class Livro
+ class Livro
 {
-  
+    public  string Id {get; private set;} 
+    public  string Titulo {get; private set;} 
+    public  string Autor {get; private set;} 
+    public bool EstaDisponivel {get; private set;} 
+
+    public Livro(string id, string titulo, string autor)
+  {
+    ArgumentNullException.ThrowIfNull(id);
+    ArgumentNullException.ThrowIfNull(titulo);
+    ArgumentNullException.ThrowIfNull(autor);
+    Id = id;
+    Titulo = titulo;
+    Autor = autor;
+    EstaDisponivel = true;
+
+  }
+
+  public void Emprestar()
+  {
+    if (EstaDisponivel)
+    {
+      EstaDisponivel = false;
+    }
+    else
+    {
+      throw new Exception ("Livro não está disponível");
+    }
+  }
+
+  public void Devolver()
+  {
+     if (!EstaDisponivel)
+    {
+      EstaDisponivel = true;
+    }
+    else
+    {
+      throw new Exception ("Livro já foi devolvido");
+
+    }
+  }
+   
 }
